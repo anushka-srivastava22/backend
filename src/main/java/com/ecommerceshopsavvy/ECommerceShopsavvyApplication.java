3cc8c36c47aa73a664ee9cdeec1ccfd04d758b19
anushka-srivastava22 <srivastavaanushka3@gmail.com>
2023-07-09 20:31:58 +0530

    first commit

---
+++ src/main/java/com/ecommerceshopsavvy/ECommerceShopsavvyApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerceshopsavvy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceShopsavvyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceShopsavvyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerceshopsavvy/ECommerceShopsavvyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceshopsavvy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceShopsavvyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
